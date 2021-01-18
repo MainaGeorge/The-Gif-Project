e6124051913278abedcec8f73c3ccf74c6047aa3
MainaGeorge <nuriszka@gmail.com>
2021-01-18 12:10:00 +0100

    added alternate ui interface

---
+++ FrontEnd/src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import {LoginComponent} from './login/login.component';
+import {HomeComponent} from './home/home.component';
+import {RegistrationComponent} from './registration/registration.component';
+
+
+const routes: Routes = [
+  {path: 'login', component: LoginComponent},
+  {path: 'home', component: HomeComponent},
+  {path: 'register', component: RegistrationComponent},
+  {path: '', redirectTo: '/home', pathMatch: 'full'}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
